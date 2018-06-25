341bca5bdc5c59206b84644051fa11017e4fd9e8
Patricia Nykaza <pnykaza@calstatela.edu>
2018-06-25 19:53:18 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(ana_ray)
+    result = []
+    comparison = word.split("").sort
+    ana_ray.each {|i|
+      if i.split("").sort === comparison
+        result.push(i)
+      end
+    }
+    result
+  end
+  
+end
